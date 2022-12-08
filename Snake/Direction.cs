fc6c19035b77fca834ab456a1ec179d9721bf7d7
Bruno Marazzi <brunomarazzidev@gmail.com>
2022-12-08 12:47:57 -0300

    First commit

---
+++ Snake/Direction.cs
@@ -0,0 +1,19 @@
+﻿namespace Snake
+{
+    public class Direction
+    {
+        public readonly static Direction Left = new(0, -1);
+        public readonly static Direction Right = new(0, 1);
+        public readonly static Direction Up = new(-1, 0);
+        public readonly static Direction Down = new(1, 0);
+
+        public int RowOffset { get; }
+        public int ColumnOffset { get; }
+
+        private Direction(int rowOffset, int columnOffset)
+        {
+            RowOffset = rowOffset;
+            ColumnOffset = columnOffset;
+        }
+    }
+}
